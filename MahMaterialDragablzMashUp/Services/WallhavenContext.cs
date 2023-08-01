5c461fd0e64bb2ffa4d6695fdcc0904287ab9b34
AngkorW <AngkorW@ANGKORW-DEV>
2023-08-01 17:48:40 +0800

    WallhavenContext

---
+++ MahMaterialDragablzMashUp/Services/WallhavenContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageStudio.Services
+{
+    public class WallhavenContext : DbContext
+    {
+        public WallhavenContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => base.OnConfiguring(optionsBuilder);
+
+    }
+}
