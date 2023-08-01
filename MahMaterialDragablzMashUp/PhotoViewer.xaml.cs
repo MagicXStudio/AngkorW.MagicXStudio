c84695113fdf13a4cd81e7833b62d065027c9a99
AngkorW <AngkorW@ANGKORW-DEV>
2023-08-01 15:52:38 +0800

    PhotoViewer

---
+++ MahMaterialDragablzMashUp/PhotoViewer.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahMaterialDragablzMashUp;
+
+namespace MahAppsDragablzDemo
+{
+    /// <summary>
+    /// PhotoViewer.xaml 的交互逻辑
+    /// </summary>
+    public partial class PhotoViewer : UserControl
+    {
+        public PhotoViewer()
+        {
+            InitializeComponent();
+            DataContext = new PhotoViewerViewModel();
+        }
+    }
+}
---
+++ MahMaterialDragablzMashUp/PhotoViewerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using MahApps.Metro.Controls;
+
+namespace MahMaterialDragablzMashUp
+{
+    public class PhotoViewerViewModel : INotifyPropertyChanged
+    {
+        public ICommand MouseMoveCommand { get; }
+
+        public ICommand MouseWheelCommand { get; }
+
+        public ICommand DragOverCommand { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public PhotoViewerViewModel()
+        {
+            MouseMoveCommand = new AnotherCommandImplementation((o) => MouseMoveHandler(o));
+            MouseWheelCommand = new AnotherCommandImplementation(_ => MouseWheelHandler());
+            DragOverCommand = new AnotherCommandImplementation(_ => DragOverHandler());
+        }
+
+        public Flyout? LeftFlyout { get; set; }
+
+        private void MouseMoveHandler(object obj)
+        {
+
+        }
+
+        private void MouseWheelHandler()
+        {
+
+        }
+
+        private void DragOverHandler()
+        {
+        }
+    }
+}
