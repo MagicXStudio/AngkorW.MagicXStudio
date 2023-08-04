6ae6a01eeac9aada15240e9d557bccc3cc4898cc
AngkorW <AngkorW@ANGKORW-DEV>
2023-08-04 11:53:35 +0800

    .

--- MahMaterialDragablzMashUp/Services/WallhavenContext.cs
+++ MahMaterialDragablzMashUp/Services/WallhavenContext.cs
@@ -23,7 +23,7 @@ namespace ImageStudio.Services
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlite(@"Data Source=Awesome.db;");
+            optionsBuilder.UseSqlite(@"Data Source=./Awesome.db;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
